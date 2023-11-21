e6c75c2c608cf3612f115440a223e1a1f52ee870
Aryans213 <149963528+Aryans213@users.noreply.github.com>
2023-11-20 22:51:55 -0600

    Update SpeakHello.js

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -13,7 +13,7 @@
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 (function(window){
-  var speakWord = "Good Bye";
+  var speakWord = "Hello";
   var helloSpeaker = {};
   
   helloSpeaker.speak = function speak(name) {
